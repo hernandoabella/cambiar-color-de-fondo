787a480dc691869d10901ee2e9dd70fed8a2cd43
Hernando Abella <hernandoabella@gmail.com>
2023-07-24 21:58:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,37 +4,38 @@ let colorCode = document.querySelector(".hex");
 
 const hexNumbers = ["A", "B", "C", "D", "E", "F", 1, 2, 3, 4, 5, 6];
 
-hexBtn.addEventListener(
-  "click",
-  (randomColor = () => {
-    let color = "#";
+hexBtn.addEventListener("click", randomColor);
 
-    for (let i = 0; i < 6; i++) {
-      let random = Math.floor(Math.random() * hexNumbers.length);
-      color += hexNumbers[random];
-    }
+function randomColor() {
+  let color = "#";
 
-    bgColor.style.backgroundColor = color;
-    colorCode.innerHTML = color;
-  })
-);
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    color += hexNumbers[random];
+  }
+
+  bgColor.style.backgroundColor = color;
+  colorCode.innerHTML = color;
+}
 
 document.getElementById("copyBtn").addEventListener("click", function () {
   const hexCodeElement = document.querySelector(".hex");
   const hexCode = hexCodeElement.innerText;
 
-  navigator.clipboard
-    .writeText(hexCode)
-    .then(function () {
-      // Mostrar mensaje "Copiado al portapapeles"
-      hexCodeElement.innerText = "¡Copiado!";
+  const tempInput = document.createElement("input");
+  tempInput.setAttribute("value", hexCode);
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999); // For mobile devices
 
-      // Esperar 2 segundos y volver a mostrar el código original
-      setTimeout(function () {
-        hexCodeElement.innerText = hexCode;
-      }, 1000);
-    })
-    .catch(function (error) {
-      console.error("Error al copiar al portapapeles:", error);
-    });
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+
+  // Mostrar mensaje "Copiado al portapapeles"
+  hexCodeElement.innerText = "¡Copiado!";
+
+  // Esperar 2 segundos y volver a mostrar el código original
+  setTimeout(function () {
+    hexCodeElement.innerText = hexCode;
+  }, 1000);
 });
